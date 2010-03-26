a314ab94cf7b1c512ae38e639be6d763e41e474d
Jacqui Maher <jacqui@brighter.net>
2010-03-26 16:21:39 -0400

    Initial commit to hdcloud_api.

---
+++ test/test_hdcloud_api.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestHdcloudApi < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
